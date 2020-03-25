d3032babd1e3a204c23a975fe98ae26cf21fe298
Ederson Ferreira <edersondeveloper@gmail.com>
2020-03-25 00:34:16 -0400

    testing ong creator

--- backend/src/controllers/ongController.js
+++ backend/src/controllers/ongController.js
@@ -4,12 +4,11 @@ const crypto = require('crypto')
 module.exports = {
     async index(request, response){
         const ongs = await connection('ongs').select('*')
-    
         return response.json(ongs)
     },
     async create(request, response){
         const {name, email, whatsapp, city, uf} = request.body
-
+        
         const id = crypto.randomBytes(4).toString('HEX')
     
         await connection('ongs').insert({
